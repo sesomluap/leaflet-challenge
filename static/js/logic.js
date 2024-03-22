ef73ee4d6e9343f68eb69dcf38dc7df7d30672f6
sesomluap <142470757+sesomluap@users.noreply.github.com>
2024-03-22 14:14:16 -0500

    added code

---
+++ static/js/logic.js
@@ -0,0 +1,78 @@
+// Create variable for the URL data link
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson"
+// Create map
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+});
+
+// Add tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+// Retrieve and map the earthquake data
+d3.json(url).then(function(data) {
+    function mapStyle(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: mapColor(feature.geometry.coordinates[2]),
+            color: "black",
+            radius: mapRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+        }
+        // Establish colors for depth
+        function mapColor(depth) {
+            switch(true) {
+                case depth > 90:
+                    return "red";
+                case depth > 70:
+                    return "orangered";
+                case depth > 50:
+                    return "orange";
+                case depth > 30:
+                    return "gold";
+                case depth > 10:
+                    return "yellow";
+                default:
+                    return "lightgreen;"
+            }
+        }
+        // Establidh magnitude size
+        function mapRadius(mag) {
+            if (mag === 0) {
+                return 1;
+            }
+
+            return mag * 4;
+        } 
+        // Add earthquake data to the map
+        L.geoJson(data, {
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng);
+            },
+            style: mapStyle,
+
+            // Activate pop-up data on click
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place + "<br>Depth: " + feature.geometry.coordinates[2]);
+            }
+        }).addTo(myMap);
+
+// Add the legend with colors correlating to depth
+var legend = L.control({position: "bottomright"});
+legend.onAdd =  function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
+
+    for (var i = 0; i < depth.length; i++) {
+        div.innerHTML +=
+        '<i style="background:' + mapColor(depth[i] + 1) + '"></i> ' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(myMap)
+});
